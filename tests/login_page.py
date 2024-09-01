5fb91deeb1b963600207581ac2560067b2dfd80e
saritvakrat <sarit.vakrat@glassboxdigital.com>
2024-09-01 19:58:50 +0300

    fix async errors

--- tests/login_page.py
+++ tests/login_page.py
@@ -1,50 +1,38 @@
 import pytest
-from playwright.async_api import async_playwright
 from pages.home_page import HomePage
-
+from utils import BrowserManager
 
 @pytest.fixture(scope="function")
-async def setup():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        page = await browser.new_page()
-        home_page = HomePage(page)
-        home_page.navigate()
-        yield home_page
-        await browser.close()
+def setup():
+    manager = BrowserManager(headless=False)
+    browser = manager.start_browser()
+    page = browser.new_page()
+    home_page = HomePage(page)
+    home_page.navigate()
+    yield home_page
+    page.close()
+    manager.stop_browser()
 
-
-@pytest.fixture(scope="function", autouse=True)
-async def teardown(setup):
-    page = setup.page
-    await page.close()
-
-
-@pytest.mark.asyncio
-async def test_successful_login(setup):
+def test_successful_login(setup):
     home_page = setup
     home_page.login('standard_user', 'secret_sauce')
 
     # Assert that the login was successful by checking the URL
-    await home_page.page.wait_for_url('https://www.saucedemo.com/v1/inventory.html')
+    home_page.page.wait_for_url('https://www.saucedemo.com/v1/inventory.html')
     assert home_page.page.url == 'https://www.saucedemo.com/v1/inventory.html'
 
-
-@pytest.mark.asyncio
-async def test_unsuccessful_login(setup):
+def test_unsuccessful_login(setup):
     home_page = setup
     home_page.login('invalid_user', 'invalid_password')
 
     # Assert that an error message is displayed
-    assert await home_page.is_error_message_visible()
-    assert "Epic sadface: Username and password do not match any user in this service" in await home_page.get_error_message()
+    assert home_page.is_error_message_visible()
+    assert "Epic sadface: Username and password do not match any user in this service" in home_page.get_error_message()
 
-
-@pytest.mark.asyncio
-async def test_login_with_empty_fields(setup):
+def test_login_with_empty_fields(setup):
     home_page = setup
     home_page.login('', '')
 
     # Assert that an error message is displayed
-    assert await home_page.is_error_message_visible()
-    assert "Epic sadface: Username is required" in await home_page.get_error_message()
+    assert home_page.is_error_message_visible()
+    assert "Epic sadface: Username is required" in home_page.get_error_message()
